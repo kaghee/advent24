9c3f13ce8bca699db4f24ec06e36d4a4417dc0f6
Agnes Kassai <kass.agnes@gmail.com>
2025-01-06 19:18:35 +0800

    feat: add level 7-2

--- levels/lvl7.py
+++ levels/lvl7.py
@@ -5,30 +5,45 @@ class BridgeRepair:
     def __init__(self, lines=None):
         self.lines = lines
 
-    def process_next_num(self, value, base, rest_of_nums):
+    def can_produce_value(self, value, base, rest_of_nums, with_concat=False):
+        """ Takes a value, a base number and a list of numbers.
+        It returns True if value can be obtained by adding, multiplying
+        and optionally even concatenating the base number with the next
+        in the rest of the numbers."""
         is_valid_1 = False
         is_valid_2 = False
+        is_valid_3 = False
 
         if not rest_of_nums:
             return base == value
-        acc_1 = base + rest_of_nums[0]
-        acc_2 = base * rest_of_nums[0]
+
+        acc_1 = base + rest_of_nums[0]  # Addition
+        acc_2 = base * rest_of_nums[0]  # Multiplication
+        acc_3 = int(f"{base}{rest_of_nums[0]}")     # Concatenation
+
         if acc_1 <= value:
-            is_valid_1 = self.process_next_num(value, acc_1, rest_of_nums[1:])
+            is_valid_1 = self.can_produce_value(
+                value, acc_1, rest_of_nums[1:], with_concat=with_concat)
         if acc_2 <= value:
-            is_valid_2 = self.process_next_num(value, acc_2, rest_of_nums[1:])
+            is_valid_2 = self.can_produce_value(
+                value, acc_2, rest_of_nums[1:], with_concat=with_concat)
+        if acc_3 <= value:
+            is_valid_3 = self.can_produce_value(
+                value, acc_3, rest_of_nums[1:], with_concat=with_concat)
 
-        return is_valid_1 or is_valid_2
+        if with_concat:
+            return is_valid_1 or is_valid_2 or is_valid_3
+        else:
+            return is_valid_1 or is_valid_2
 
-    def run_first_task(self):
+    def run_task(self, with_concat=False):
         counter = 0
         for line in self.lines:
-            print(line)
             value, numbers = int(
                 line[:line.index(":")]), line[line.index(":") + 2:]
             numbers = [int(n) for n in numbers.split(" ")]
 
-            if self.process_next_num(value, numbers[0], numbers[1:]):
+            if self.can_produce_value(value, numbers[0], numbers[1:], with_concat):
                 counter += value
 
         print("Result:", counter)
@@ -36,6 +51,9 @@ class BridgeRepair:
 
 def run_first_task(file_name):
     lines = get_file_lines(file_name)
+    BridgeRepair(lines).run_task()
 
-    BridgeRepair(lines).run_first_task()
 
+def run_second_task(file_name):
+    lines = get_file_lines(file_name)
+    BridgeRepair(lines).run_task(with_concat=True)
