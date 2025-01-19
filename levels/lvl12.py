16e44be78538a2d83f67659a08478c03a42eb5ce
Agnes Kassai <kass.agnes@gmail.com>
2025-01-19 16:05:16 +0800

    feat: level 12-2 finished

--- levels/lvl12.py
+++ levels/lvl12.py
@@ -1,24 +1,8 @@
 import sys
 from input_parsers import get_line_chars, parse_input
 
-# total price 140
-# EXAMPLE = """\
-# AAAA
-# BBCD
-# BBCC
-# EEEC\
-# """
 
-# total price 772
-# EXAMPLE = """\
-# OOOOO
-# OXOXO
-# OOOOO
-# OXOXO
-# OOOOO\
-# """
-
-# total price 1930
+# total price 1930 / 1206
 EXAMPLE = """\
 RRRRIICCFF
 RRRRIICCCF
@@ -32,112 +16,128 @@ MIIISIJEEE
 MMMISSJEEE\
 """
 
-
 class GardenGroups:
     def __init__(self, garden):
         self.garden = garden
-        self.groups_info = {}
         self.graph = {}
-        self.unique_key = 0
-        self.is_new_group = False
-        self.DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)
-                           ]  # up, right, down, left
+        self.DIR_DIFFS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
     def add_node(self, node):
         if node not in self.graph:
             self.graph[node] = []
 
-    def add_edge(self, node_1, node_2):
-        if node_1 in self.graph and node_2 in self.graph:
-            self.graph[node_1].append(node_2)
-            self.graph[node_2].append(node_1)
+    def add_edge(self, node_1, node_2, weight):
+        node_2_coords = (node_2[0], node_2[1])
+        if node_1 in self.graph and node_2_coords in self.graph:
+            self.graph[node_1].append((node_2_coords, weight))
+            self.graph[node_2_coords].append((node_1, weight))
 
     def is_on_map(self, cell: tuple[int, int]) -> bool:
         """ Checks if the cell is within the map boundaries. """
         size = len(self.garden)
         return cell[0] >= 0 and cell[1] >= 0 and cell[0] < size and cell[1] < size
 
-    def is_valid_group_member(self, a: tuple[int, int], b: tuple[int, int]) -> bool:
-        """ Checks if the second coordinate is within the map
-        and if it's in the same group as the first one."""
-        if not self.is_on_map(b):
-            return False
+    def are_same_group(self, a, b):
+        """ Checks if two cells belong to the same group. """
+        return self.garden[a[0]][a[1]] == self.garden[b[0]][b[1]]
 
-        group = self.garden[a[0]][a[1]]
-        other_group = self.garden[b[0]][b[1]]
-        return group == other_group
-
-    def get_neighbours_in_group(self, cell: tuple[int, int]):
-        """ Gathers all neighbour coordinates that belong
-        to the same group as the cell. """
+    def get_neighbours(self, cell: tuple[int, int], in_same_group: bool = False):
+        """ Gathers all neighbour coordinates of a cell. """
         neighbours = []
 
-        for diff in self.DIRECTIONS:
+        for diff in self.DIR_DIFFS:
             next_cell = (cell[0] + diff[0], cell[1] + diff[1])
-            if self.is_valid_group_member(cell, next_cell):
-                neighbours.append(next_cell)
+            neighbours.append(next_cell if self.is_on_map(next_cell) else None)
 
-        return neighbours
+        if in_same_group:
+            return [nb if nb and self.are_same_group(cell, nb) else None for nb in neighbours]
+        else:
+            return neighbours
+
+    def get_discount(self, cell, next_cell):
+        """ Calculates how much discount a pair of cells will get,
+        based on the number of sides they share on the map (i.e.
+        if they're on the edge of the map or if they have non-group cells
+        on their same side.)"""
+        cell_neighbours = [nb for nb in self.get_neighbours(
+            cell, in_same_group=True)]
+        next_cell_neighbours = [nnb for nnb in self.get_neighbours(
+            next_cell, in_same_group=True)]
+
+        discount = len(
+            [i for i, j in zip(cell_neighbours, next_cell_neighbours) if i is None and j is None])
+        return discount
 
     def add_nodes_to_graph(self, cell, neighbours):
+        """ Adds node and edges to the graph based on neighbours.
+        Discounts are stored as edge weights."""
         self.add_node(cell)
-        for neighbour in neighbours:
-            self.add_edge(cell, neighbour)
+        same_group_neighbours = [
+            nb for nb in neighbours if nb and self.are_same_group(cell, nb)]
+
+        for nb_cell in same_group_neighbours:
+            discount = self.get_discount(cell, nb_cell)
+            self.add_edge(cell, nb_cell, discount)
 
     def dfs(self, cell: tuple[int, int], visited: set):
-        """ Checks the neighbouring cells and increases the area and number of fences
-        for every cell based on how many neighbour cells belong to the same group."""
-        if cell not in visited:
-            neighbours = self.get_neighbours_in_group(cell)
-            current_group = self.garden[cell[0]][cell[1]]
-
-            if current_group not in self.groups_info:
-                # We are in a new group. Add a new entry to our data.
-                self.groups_info[current_group] = {
-                    "area": 1, "fences": 4 - len(neighbours)}
-            elif self.is_new_group:
-                # We are in a new group but we already covered a group with this name.
-                # Alter the previous group's name before storing this new one.
-                self.unique_key += 1
-                self.groups_info[self.unique_key] = self.groups_info[current_group]
-                del self.groups_info[current_group]
-
-                self.groups_info[current_group] = {
-                    "area": 1, "fences": 4 - len(neighbours)}
-            else:
-                # We are still within the current group. Increase the area and fences.
-                self.groups_info[current_group]["area"] += 1
-                self.groups_info[current_group]["fences"] += 4 - \
-                    len(neighbours)
+        """ Returns all the nodes that belong to a group
+        by checking the neighbouring cells. """
+        nodes_in_group = []
+        if cell not in visited and cell is not None:
+            neighbours = [nb for nb in self.get_neighbours(
+                cell, in_same_group=True) if nb is not None]
 
             visited.add(cell)
-            self.is_new_group = False
+            nodes_in_group.append(cell)
 
             for neighbour in neighbours:
-                self.dfs(neighbour, visited)
+                nbs = self.dfs(neighbour, visited)
+                if nbs:
+                    nodes_in_group.extend(nbs)
+
+            return nodes_in_group
+
+    def calculate_price(self, groups, with_discount: bool = False):
+        total_price = 0
+        for group in groups:
+            area = len(group)
+            fences = 0
+            discount = 0
+            for node in group:
+                fences += 4 - len(self.graph[node])
+                discount += sum([n[1] for n in self.graph[node]])
+
+            if with_discount:
+                total_price += area * (fences - discount // 2)
+            else:
+                total_price += area * fences
+
+        return total_price
 
     def run_task(self):
-        visited = set()
-
-        # Create a graph from the map, where nodes are the cells and
+        # Create a graph from the map, where the cells are the nodes and
         # edges exist between nodes that make up a connected graph.
+        visited = set()
         for i, row in enumerate(self.garden):
             for j, _ in enumerate(row):
                 cell = (i, j)
-                neighbours = self.get_neighbours_in_group(cell)
+                neighbours = self.get_neighbours(cell)
                 self.add_nodes_to_graph(cell, neighbours)
 
-        # Traverse the graph recursively to collect data. Take note of
-        # every new group (= every new disconnected graph within the graph).
+        # Traverse the graph recursively to collect data. Store every
+        # new group (= every new disconnected graph within the graph).
+        groups = []
         for g in list(self.graph):
             if g not in visited:
-                self.is_new_group = True
-                self.dfs(g, visited)
+                nodes_in_group = self.dfs(g, visited)
+                groups.append(nodes_in_group)
 
-        price = sum([x["area"] * x["fences"]
-                    for x in self.groups_info.values()])
+        # Calculate the total price of all the groups.
+        total_price = self.calculate_price(groups)
+        discount_price = self.calculate_price(groups, with_discount=True)
 
-        print("Total price:", price)
+        print("Total price:", total_price)
+        print("Total price with discount:", discount_price)
 
 
 if __name__ == "__main__":
@@ -145,9 +145,6 @@ if __name__ == "__main__":
     input = EXAMPLE if use_example else parse_input('2024', '12')
     garden = get_line_chars(input)
 
-    print("Part I:")
+    print("Part I and II:")
     gg = GardenGroups(garden)
     gg.run_task()
-
-    # print("Part II:")
-    # gg.run_task()
